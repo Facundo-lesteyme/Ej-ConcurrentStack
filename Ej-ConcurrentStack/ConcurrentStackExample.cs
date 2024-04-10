02ce7e7fff8431ab551e7679aaf44d756980841d
Facundo-lesteyme <127899926+Facundo-lesteyme@users.noreply.github.com>
2024-04-10 17:08:41 -0300

    Update ConcurrentStackExample.cs
    Primer commt

--- Ej-ConcurrentStack/ConcurrentStackExample.cs
+++ Ej-ConcurrentStack/ConcurrentStackExample.cs
@@ -10,27 +10,33 @@ namespace Ej_ConcurrentStack
         //Inserta un objeto al principio
         public void PushToStack(int value)
         {
-            cs.Push(value);
+        cs.Push(value);
         }
 
         //Intenta extraer y devolver el objeto situado al principio
         public int PopFromStack()
         {
-            int result;
-            cs.TryPop(out result);
-            return result;
+        int result;
+        cs.TryPop(out result);
+        return result;
         }
-
+    
         // Intenta devolver un objeto situado al principio
-        public bool TryPeek(out int result)
+        public bool PeekFromStack(out int result)
         {
-            return cs.TryPeek(out result);
+        return cs.TryPeek(out result);
         }
 
         //Cuenta el número de elementos actualmente almacenados
         public int Count()
         {
-            return cs.Count;
+        return cs.Count;
         }
-    }
-}
+
+        // Método para obtener todos los elementos de la pila sin eliminarlos
+        public int[] ShowAllItemsFromStack()
+        {
+        return cs.ToArray();
+        }
+        }
+        }
